d360d66766459a690191b1c797ed5487abefba05
jpolfer <jonathan.polfer@gmail.com>
2016-08-05 15:57:47 -0500

    Fixed launch button code to meet Content Security Policies.

--- TestCordovaApp/www/scripts/index.js
+++ TestCordovaApp/www/scripts/index.js
@@ -10,7 +10,8 @@
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
+        document.addEventListener('resume', onResume.bind(this), false);
+        document.querySelector('input#launch').addEventListener('click', launchGoogle);
     };
 
     function onPause() {
